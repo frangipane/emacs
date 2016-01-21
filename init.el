2de42adaaa0e1466739ea1b5792f759bf1ad1f5b
frangipane <morgengruss@gmail.com>
2016-01-21 12:02:14 -0800

    create init.el - replaces .emacs

---
+++ init.el
@@ -0,0 +1,14 @@
+;; path where settings files are kept
+(add-to-list 'load-path "~/.emacs.d/settings")
+
+;; path to where plugins are kept
+(setq plugin-path "~/.emacs.d/el-get/")
+
+;; define various custom functions
+;(require 'custom-functions)
+
+;; configure general settings
+;(require 'general-settings)
+
+;; install dependencies with el-get
+(require 'el-get-settings)
