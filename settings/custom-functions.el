8717bef3d115fc9469e21a70581fc8619b6aa71a
frangipane <morgengruss@gmail.com>
2016-02-05 12:45:54 -0800

    add custom function def

--- settings/custom-functions.el
+++ settings/custom-functions.el
@@ -9,4 +9,13 @@
 (defun include-plugin (plugin)
   (add-to-list 'load-path (make-plugin-path plugin)))
 
+(defun make-elget-path (plugin)
+  (expand-file-name
+   (concat elget-path plugin)))
+
+(defun include-elget-plugin (plugin)
+  (add-to-list 'load-path (make-elget-path plugin)))
+
 (provide 'custom-functions)
+
+
