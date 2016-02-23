168b79b55c84774e2187f5fffd621a340d2fbf62
frangipane <morgengruss@gmail.com>
2016-02-23 09:07:45 -0800

    rm manual set exec-path

--- settings/general-settings.el
+++ settings/general-settings.el
@@ -2,15 +2,15 @@
 ;;; General or Global Settings ;;;
 ;--------------------------------;
 
-; set PATH, because we don't load .bashrc
+; OS X: set PATH, because we don't load .bashrc
 ; function from https://gist.github.com/jakemcc/3887459
-(defun set-exec-path-from-shell-PATH ()
-  (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
-  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo -n $PATH'")))
-    (setenv "PATH" path-from-shell)
-    (setq exec-path (split-string path-from-shell path-separator))))
+;; (defun set-exec-path-from-shell-PATH ()
+;;   (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
+;;   (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo -n $PATH'")))
+;;     (setenv "PATH" path-from-shell)
+;;     (setq exec-path (split-string path-from-shell path-separator))))
 
-(if window-system (set-exec-path-from-shell-PATH))
+;; (if window-system (set-exec-path-from-shell-PATH))
 
 ; language
 (setq current-language-environment "English")
