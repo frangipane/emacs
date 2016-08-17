977d0eaa1c20f751a444de960bde53e3b9b75d62
frangipane <morgengruss@gmail.com>
2016-08-17 12:01:44 -0700

    ipython 5.0.0 not compatible w/ inferior shell -- disable

--- settings/python-settings.el
+++ settings/python-settings.el
@@ -13,7 +13,7 @@
 (setq elpy-rpc-backend "jedi")
 
 ;; use IPython REPL instead of standard Python REPL integration
-(elpy-use-ipython)
+;;(elpy-use-ipython)
 
 ;; Emacs+elpy comes with package called Flymake to support syntax checking.
 ;; Switch out Flymake for Flycheck, which supports realtime syntax checking.
